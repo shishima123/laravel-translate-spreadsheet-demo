f3239b0ed1ae7a46151ab79ff4e8604cc5a79355
Phuoc Nguyen <shishima21@gmail.com>
2023-08-03 00:05:10 +0700

    update

--- app/Http/Controllers/TranslateController.php
+++ app/Http/Controllers/TranslateController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\SpreadsheetTranslate;
 use Illuminate\Http\Request;
+use Shishima\TranslateSpreadsheet\Facades\TranslateSpreadsheet;
 
 class TranslateController extends Controller
 {
@@ -15,7 +15,7 @@ class TranslateController extends Controller
     public function store(Request $request)
     {
         $fileInput = $request->file('file');
-        $output = SpreadsheetTranslate::translate($fileInput);
+        $output = TranslateSpreadsheet::translate($fileInput);
         return response()->download($output)->deleteFileAfterSend(true);
     }
 }
