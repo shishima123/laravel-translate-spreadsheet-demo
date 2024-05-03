e795fc16d667c70b77fe54c386cad1bf1f6f01d7
Phuoc Nguyen <shishima21@gmail.com>
2024-05-03 16:53:09 +0700

    init nativephp

--- app/Http/Controllers/TranslateController.php
+++ app/Http/Controllers/TranslateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Shishima\TranslateSpreadsheet\Facades\TranslateSpreadsheet;
 use Shishima\TranslateSpreadsheet\Enumerations\TranslateEngine;
+use Native\Laravel\Facades\Notification;
 
 set_time_limit(0);
 class TranslateController extends Controller
@@ -16,12 +17,22 @@ class TranslateController extends Controller
 
     public function store(Request $request)
     {
+        // native php
+        Notification::title('Translate Spreadsheet')
+            ->message('The file is processing...')
+            ->show();
+
         $output = TranslateSpreadsheet::setTransSource($request->source)
             ->setTransTarget($request->target)
             ->highlightSheet((bool) $request->isHighlightSheet)
             ->translateSheetName((bool) $request->isTranslateSheetName)
             ->setTranslateEngine(TranslateEngine::from($request->translateEngine))
             ->translate($request->file('file'));
+
+        // native php
+        Notification::title('Translate Spreadsheet')
+            ->message('Translate completed.')
+            ->show();
 
         return response()->download($output)->deleteFileAfterSend(true);
     }
---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Laravel\Facades\Window;
+use Native\Laravel\Contracts\ProvidesPhpIni;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Window::open()
+        ->width(500)
+        ->height(700)
+        ->resizable(false);
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     */
+    public function phpIni(): array
+    {
+        return [
+        ];
+    }
+}
